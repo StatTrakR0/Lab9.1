eac6b8721830c808986fe597f31b9a0f7633ffd8
StatTrakR0 <59964937+StatTrakR0@users.noreply.github.com>
2022-12-19 19:33:37 +0200

    Add files via upload

---
+++ Model.java
@@ -0,0 +1,23 @@
+public class Model {
+    private static Model instance = new Model();
+
+    private List<User> model;
+
+    public static Model getInstance() {
+        return instance;
+    }
+
+    private Model() {
+        model = new ArrayList<>();
+    }
+
+    public void add(User user) {
+        model.add(user);
+    }
+
+    public List<String> list() {
+        return model.stream()
+                .map(User::getName)
+                .collect(Collectors.toList());
+    }
+}
